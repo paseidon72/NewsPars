a2949d6b0f7c4e30fa5e983f522185b12a1a96d1
Andrey Postovoiy <113462667+paseidon72@users.noreply.github.com>
2023-01-09 15:37:52 +0200

    newspars

---
+++ newspars/link_id.py
@@ -0,0 +1,5 @@
+url = "https://www.securitylab.ru/news/535534.php"
+
+article_id = url.split("/")[-1]
+article_id = article_id[:-4]
+print(article_id)
---
+++ newspars/pars.py
@@ -0,0 +1,88 @@
+import json
+import requests
+from bs4 import BeautifulSoup
+from datetime import datetime
+import time
+
+
+
+def get_first_news():
+    headers = {
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 Safari/537.36"
+    }
+
+    url = "https://www.securitylab.ru/news/"
+    r = requests.get(url=url, headers=headers)
+
+    soup = BeautifulSoup(r.text, "lxml")
+    articles_cards = soup.find_all("a", class_="article-card")
+
+    news_dict = {}
+    for article in articles_cards:
+        articles_title = article.find("h2", class_="article-card-title").text.strip()
+        article_desc = article.find("p").text.strip()
+        article_url = f'https://www.securitylab.ru{article.get("href")}'
+
+        article_date_time = article.find("time").get("datetime")
+        date_from_iso = datetime.fromisoformat(article_date_time)
+        date_time = datetime.strftime(date_from_iso, "%Y-%m-%d %H:%M:%S:")
+        article_date_timestamp = time.mktime(datetime.strptime(date_time, "%Y-%m-%d %H:%M:%S:").timetuple())
+
+        article_id = article_url.split("/")[-1]
+        article_id = article_id[:-4]
+
+        #print(f"{articles_title} | {article_url} | {article_date_timestamp}")
+
+        news_dict[article_id] = {
+            "article_date_timestamp": article_date_timestamp,
+            "articles_title": articles_title,
+            "article_url": article_url,
+            "article_desc": article_desc
+        }
+
+    with open("news_dict.json", "w") as file:
+        json.dump(news_dict, file, indent=4, ensure_ascii=False)
+
+
+def check_news_update():
+    with open("news_dict.json") as file:
+        news_list = json.load(file)
+
+    print(news_list)
+
+
+
+def main():
+    #get_first_news()
+    check_news_update()
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
