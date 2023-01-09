dea14942d4bc7789d0fecd469484d7d270223b4f
Andrey Postovoiy <113462667+paseidon72@users.noreply.github.com>
2023-01-09 19:32:50 +0200

    newspars

--- newspars/link_id.py
+++ newspars/link_id.py
@@ -1,5 +1,16 @@
-url = "https://www.securitylab.ru/news/535534.php"
+# url = "https://www.securitylab.ru/news/535534.php"
+#
+# article_id = url.split("/")[-1]
+# article_id = article_id[:-4]
+# print(article_id)
+import json
 
-article_id = url.split("/")[-1]
-article_id = article_id[:-4]
-print(article_id)
+with open("news_dict.json") as file:
+    news_dict = json.load(file)
+
+search_id = "535536888"
+
+if search_id in news_dict:
+    print("Uje est")
+else:
+    print("svejak")
